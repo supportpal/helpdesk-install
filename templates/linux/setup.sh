d8da30c405eec4b953aea893f82ddbcad731f8f2
Kieran Brahney <kieran@supportpal.com>
2021-09-01 12:47:53 +0100

    fix(linux): mysql statements

--- templates/linux/setup.sh
+++ templates/linux/setup.sh
@@ -529,14 +529,14 @@ install_mysql() {
     systemd restart mysql
   fi
 
-  mysql --connect-expired-password -u"root" -p"${tmp_root}" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${root_password}'; FLUSH PRIVILEGES;"
+  mysql --connect-expired-password --user='root' --password="'${tmp_root}'" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${root_password}'; FLUSH PRIVILEGES;"
   if [[ $os_type == 'rhel' ]]; then
-    mysql -u"root" -p"${root_password}" -e "UNINSTALL COMPONENT 'file://component_validate_password';"
+    mysql --user="root" --password="'${root_password}'" -e "UNINSTALL COMPONENT 'file://component_validate_password';"
   fi
-  mysql -u"root" -p"${root_password}" -e "CREATE DATABASE ${database};"
-  mysql -u"root" -p"${root_password}" -e "CREATE USER '${username}'@'localhost' IDENTIFIED BY '$user_password';"
-  mysql -u"root" -p"${root_password}" -e "GRANT ALL PRIVILEGES ON ${username}.* TO '${database}'@'localhost';"
-  mysql -u"root" -p"${root_password}" -e "FLUSH PRIVILEGES;"
+  mysql --user="root" --password="'${root_password}'" -e "CREATE DATABASE \`${database}\`;"
+  mysql --user="root" --password="'${root_password}'" -e "CREATE USER '${username}'@'localhost' IDENTIFIED BY '$user_password';"
+  mysql --user="root" --password="'${root_password}'" -e "GRANT ALL PRIVILEGES ON \`${database}.*\` TO '${username}'@'localhost';"
+  mysql --user="root" --password="'${root_password}'" -e "FLUSH PRIVILEGES;"
 }
 
 install_supportpal() {
