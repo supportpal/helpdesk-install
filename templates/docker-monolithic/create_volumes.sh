82bc9cef1e338d33cbcebd4e34492039cdd0f052
Gaitholabi <24876890+Gaitholabi@users.noreply.github.com>
2022-05-03 19:01:29 +0300

    feat: add persistant addons directory (#81)
    * feat: add persistant addons directory
    
    * add trailing lf
    
    Co-authored-by: Kieran Brahney <kieran@supportpal.com>

---
+++ templates/docker-monolithic/create_volumes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu -o pipefail
+
+create_volume() {
+  printf "creating docker volume of name %s ... " "$1"
+  # shellcheck disable=SC2034
+  output="$(docker volume create "$1")"
+  printf "✔\n"
+}
+
+create_volume supportpal_db
+create_volume supportpal_config
+create_volume supportpal_redis
+create_volume supportpal_storage
+create_volume supportpal_logs
+create_volume supportpal_addons
