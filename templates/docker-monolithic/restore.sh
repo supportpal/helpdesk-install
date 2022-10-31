af86b757c852910b1057b00cee89c02ea2edc87a
Kieran Brahney <kieran@supportpal.com>
2022-10-31 17:13:49 +0000

    use docker compose restart instead

--- templates/docker-monolithic/restore.sh
+++ templates/docker-monolithic/restore.sh
@@ -2,6 +2,35 @@
 
 set -eu -o pipefail
 
+# usage: version_ge <installed_version> <minimum_version>
+version_ge() {
+  if ! [ "$(printf '%s\n' "$2" "$1" | sort -V | head -n1)" = "$2" ]; then
+    printf "error: %s is less than minimum required version of %s\n" "$1" "$2"
+    exit 1
+  fi
+}
+
+check_docker_compose() {
+  local min="2.2.1" version command_status
+
+  set +e
+  version="$(docker compose version --short 2>&1)"
+  command_status="$?"
+  set -e
+
+  if [ $command_status -ne 0 ]; then
+    printf "error: Install docker compose using the official installation instructions: https://docs.docker.com/compose/install/\n"
+    exit 1
+  fi
+
+  version="${version#v}"
+  printf "Checking docker compose version %s >= %s ... " "$version" "$min"
+  version_ge "$version" "$min"
+  printf "✔\n"
+}
+
+check_docker_compose
+
 LAST_BACKUP_DIR="backup"
 if [[ ! -d "${LAST_BACKUP_DIR}" ]]; then
   echo "The ${LAST_BACKUP_DIR}/ directory does not exist. Create the directory, add your backup file to it and try again."; exit 1
@@ -45,4 +74,4 @@ if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/$'; then
 fi
 
 echo "Restarting services..."
-docker exec supportpal bash -c "find -L /etc/service -maxdepth 1 -mindepth 1 -type d ! -name 'redis' ! -name 'mysql' -printf '%f\n' -exec sv start {} \;"  > /dev/null
+docker compose restart 2> /dev/null
