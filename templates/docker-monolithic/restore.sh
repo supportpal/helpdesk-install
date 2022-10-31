6599a22cd15a8b79b5219c7e224a8be10f4fcbdb
Kieran Brahney <kieran@supportpal.com>
2022-10-31 16:52:23 +0000

    update docker-monolithic/restore.sh

--- templates/docker-monolithic/restore.sh
+++ templates/docker-monolithic/restore.sh
@@ -19,19 +19,15 @@ LAST_BACKUP_FILE="$(echo "${LAST_BACKUP_FILE_PATH}" | xargs basename)"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S-%s)
 TEMP_BACKUP_DIR="/tmp/tmp-backups/${TIMESTAMP}"
 
+# Determine command path. app:restore moved to app-manager in v4.2.0
 GTE_v420="$(docker exec supportpal php -r "\$release = require '/var/www/supportpal/config/release.php'; echo (int) version_compare(\$release['version'], '4.2.0', '>=');")"
-if [[ "${GTE_v420}" = "1" ]]; then
-  echo "Command is only supported after version 4.2.0."
-  exit 1
-fi
-
-BACKUP_AND_RESTORE_MODE="$(docker exec supportpal bash -c "cat /etc/container_environment/BACKUP_AND_RESTORE_MODE")"
-if [[ "${BACKUP_AND_RESTORE_MODE}" = "0" ]]; then
-  echo "Backup and restore mode must be enabled.."
-  exit 1
-fi
+if [[ "$GTE_v420" = "0" ]]; then COMMAND_PATH="/var/www/supportpal"; else COMMAND_PATH="/var/www/supportpal/app-manager"; fi
 
 echo "Found ${LAST_BACKUP_FILE}..."
+
+echo "Stopping services..."
+docker exec supportpal bash -c "find -L /etc/service -maxdepth 1 -mindepth 1 -type d ! -name 'redis' ! -name 'mysql' -printf '%f\n' -exec sv stop {} \;"  > /dev/null
+
 echo "Restoring..."
 
 docker exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}"
@@ -47,3 +43,6 @@ if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/$'; then
   docker exec supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy/caddy /"
   rm -rf "backup/${TIMESTAMP}/"
 fi
+
+echo "Restarting services..."
+docker exec supportpal bash -c "find -L /etc/service -maxdepth 1 -mindepth 1 -type d ! -name 'redis' ! -name 'mysql' -printf '%f\n' -exec sv start {} \;"  > /dev/null
