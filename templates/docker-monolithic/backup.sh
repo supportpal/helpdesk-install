0c70846b494d96fb502e3a7a700d1d3fe70ec2f9
Ghaith Olabi <ghaith@supportpal.com>
2023-08-29 21:37:20 +0200

    fix: run backup and restore with root

--- templates/docker-monolithic/backup.sh
+++ templates/docker-monolithic/backup.sh
@@ -28,17 +28,17 @@ DB_FILE_NAME=$(echo "${DB_BACKUP_PATH}" | xargs basename)
 docker exec supportpal bash -c "mv ${DB_BACKUP_PATH} ${TEMP_BACKUP_DIR}/"
 
 echo 'Backing up volume data...'
-docker exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/cache && cp -r /redis-data/ ${TEMP_BACKUP_DIR}/volumes-monolithic/cache"
-docker exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy && cp -r /caddy/ ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy"
-docker exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch && cp -r /meilisearch/ ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch"
+docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/cache && cp -r /redis-data/ ${TEMP_BACKUP_DIR}/volumes-monolithic/cache"
+docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy && cp -r /caddy/ ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy"
+docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch && cp -r /meilisearch/ ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch"
 
 echo 'Combining backups...'
-docker exec supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -czf ${APP_BACKUP_NAME} ${FILESYSTEM_BACKUP_NAME} ${DB_FILE_NAME} volumes-monolithic/"
+docker exec -u root supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -czf ${APP_BACKUP_NAME} ${FILESYSTEM_BACKUP_NAME} ${DB_FILE_NAME} volumes-monolithic/"
 
 echo 'Copying backup to host...'
 mkdir -p "${BACKUP_DIR}/"
 docker cp "supportpal:${TEMP_BACKUP_DIR}/${APP_BACKUP_NAME}" "${BACKUP_DIR}/"
-docker exec supportpal bash -c "rm -rf ${TEMP_BACKUP_DIR}/"
+docker exec -u root supportpal bash -c "rm -rf ${TEMP_BACKUP_DIR}/"
 
 echo "Restarting services..."
 docker restart supportpal 2> /dev/null
