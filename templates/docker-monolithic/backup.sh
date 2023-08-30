db8d3fc4b4a2d02e5ff9149b4dc431900c0f399d
Kieran Brahney <kieran@supportpal.com>
2023-08-30 14:59:38 +0100

    fix monolithic test

--- templates/docker-monolithic/backup.sh
+++ templates/docker-monolithic/backup.sh
@@ -30,7 +30,7 @@ docker exec supportpal bash -c "mv ${DB_BACKUP_PATH} ${TEMP_BACKUP_DIR}/"
 echo 'Backing up volume data...'
 docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/cache && cp -r /redis-data/ ${TEMP_BACKUP_DIR}/volumes-monolithic/cache"
 docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy && cp -r /caddy/ ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy"
-if [[ "$(docker exec -u root supportpal bash -c "test -d /meilisearch")" ]]; then
+if docker exec -u root supportpal bash -c "test -d /meilisearch"; then
   docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch && cp -r /meilisearch/ ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch"
 fi
 
