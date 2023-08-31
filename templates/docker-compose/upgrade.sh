f48723b3d14b879de851c7bc63471de89e393ccc
Ghaith Olabi <ghaith@supportpal.com>
2023-08-31 08:24:07 +0200

    fix(docker): run updated upgrade script

---
+++ templates/docker-compose/upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+${MAKE} create_volumes
+${DOCKER_COMPOSE_BIN} ${COMPOSE_FILES} up -d supportpal
+${DOCKER_BIN} exec -it ${WEB_SERVICE_NAME} bash /scripts/upgrade-helpdesk.sh
+${MAKE} start
