864da39ecfdde254f9ae0ea0dab1e16a2f5d229c
Kieran Brahney <kieran@supportpal.com>
2024-07-16 18:31:40 +0100

    fix restore on docker monolithic

--- templates/docker-monolithic/restore.sh
+++ templates/docker-monolithic/restore.sh
@@ -45,7 +45,7 @@ if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/$'; then
     docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch /"
   fi
   if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/qdrant'; then
-    docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant /"
+    docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant/qdrant /"
   fi
   rm -rf "backup/${TIMESTAMP}/"
 fi
