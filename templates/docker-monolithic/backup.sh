a309e578af2e3ec72fd7a62f39c5b8927881c1f9
Kieran Brahney <kieran@supportpal.com>
2025-04-25 10:36:59 +0100

    feat(docker-monolithic): include docker files in backup/restore

--- templates/docker-monolithic/backup.sh
+++ templates/docker-monolithic/backup.sh
@@ -3,17 +3,19 @@ set -eu -o pipefail
 
 usage="Options:
     -h,--help                  Display this help and exit.
-
     --online                   Do not restart the services.
+    --debug                    Enable verbose output.
 "
 
 # Options
 online=false
+debug=false
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
   -h|--help) echo "$usage" ; exit 0 ;;
   --online) online=true ;;
+  --debug) debug=true ;;
   *)
     echo "Unknown parameter passed: $1"
     exit 1
@@ -23,54 +25,98 @@ while [[ "$#" -gt 0 ]]; do
   shift
 done
 
+if $debug; then
+  set -x
+fi
+
+execute_command() {
+    local cmd="$1"
+
+    if [ "$debug" = false ]; then
+        eval "$cmd > /dev/null 2>&1"
+    else
+        eval "$cmd"
+    fi
+}
+
 BACKUP_DIR="backup"
+ABS_BACKUP_PATH="$(pwd)/${BACKUP_DIR}"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 TEMP_BACKUP_DIR="/tmp/tmp-backups/${TIMESTAMP}"
 FILESYSTEM_BACKUP_NAME="filesystem-${TIMESTAMP}.tar.gz"
 APP_BACKUP_NAME="app-${TIMESTAMP}.tar.gz"
 
+# Make sure that the script is executed in the directory where docker-compose.yml is.
+if ! docker compose config 2>/dev/null | grep -q "container_name: supportpal"; then
+    echo "error: the script must be executed in your SupportPal installation directory (where your docker-compose.yml file is)."
+    exit 1
+fi
+
+echo "[NOTICE]"
+echo "The backup includes all files in the current working directory ($(pwd))."
+echo
+echo "It is recommended to move your installation files (docker-compose.yml, docker-compose.override.yml, .env, etc) to"
+echo "a separate directory, to avoid including unnecessary files in the backup."
+echo
+
 # Determine command path. Some commands moved to app-manager in v4.2.0
-GTE_v420="$(docker exec supportpal php -r "\$release = require '/var/www/supportpal/config/release.php'; echo (int) version_compare(\$release['version'], '4.2.0', '>=');")"
+GTE_v420="$(docker compose exec supportpal php -r "\$release = require '/var/www/supportpal/config/release.php'; echo (int) version_compare(\$release['version'], '4.2.0', '>=');")"
 if [[ "$GTE_v420" = "0" ]]; then COMMAND_PATH="/var/www/supportpal"; else COMMAND_PATH="/var/www/supportpal/app-manager"; fi
 
 if ! $online; then
   echo "Stopping services..."
-  docker exec supportpal bash -c "sudo find -L /etc/service -maxdepth 1 -mindepth 1 -type d ! -name 'redis' ! -name '00redis' ! -name 'mysql' ! -name '00mysql' -printf '%f\n' -exec sv stop {} \;"
+  docker compose exec supportpal bash -c "
+    sudo find -L /etc/service -maxdepth 1 -mindepth 1 -type d \
+    ! -name 'redis' ! -name '00redis' ! -name 'mysql' ! -name '00mysql' \
+    -printf '%f\n' -exec sv stop {} \; $(if [ "$debug" = false ]; then echo '> /dev/null 2>&1'; fi)
+  "
 fi
 
 echo 'Backing up filesystem...'
-docker exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}/config/production" # create the farthest directory
-docker exec supportpal bash -c "cp -r /var/www/supportpal/config/production ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}/config"
-docker exec supportpal bash -c "cp -r /var/www/supportpal/storage ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}"
-docker exec supportpal bash -c "cp -r /var/www/supportpal/addons ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}"
-docker exec supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -czf ${FILESYSTEM_BACKUP_NAME} filesystem-${TIMESTAMP}"
+docker compose exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}/config/production" # create the farthest directory
+docker compose exec supportpal bash -c "cp -r /var/www/supportpal/config/production ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}/config"
+docker compose exec supportpal bash -c "cp -r /var/www/supportpal/storage ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}"
+docker compose exec supportpal bash -c "cp -r /var/www/supportpal/addons ${TEMP_BACKUP_DIR}/filesystem-${TIMESTAMP}"
+docker compose exec supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -czf ${FILESYSTEM_BACKUP_NAME} filesystem-${TIMESTAMP}"
 
 echo 'Backing up database...'
-DB_BACKUP_PATH=$(docker exec supportpal bash -c "cd ${COMMAND_PATH} && php artisan db:backup --store-local | grep -oE '/var/www/supportpal/.*/database-.*'")
+DB_BACKUP_PATH=$(docker compose exec supportpal bash -c "cd ${COMMAND_PATH} && php artisan db:backup --store-local | grep -oE '/var/www/supportpal/.*/database-.*'")
 DB_FILE_NAME=$(echo "${DB_BACKUP_PATH}" | xargs basename)
-docker exec supportpal bash -c "mv ${DB_BACKUP_PATH} ${TEMP_BACKUP_DIR}/"
+docker compose exec supportpal bash -c "mv ${DB_BACKUP_PATH} ${TEMP_BACKUP_DIR}/"
 
 echo 'Backing up volume data...'
-docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/cache && cp -r /redis-data/ ${TEMP_BACKUP_DIR}/volumes-monolithic/cache"
-docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy && cp -r /caddy/ ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy"
-if docker exec -u root supportpal bash -c "test -d /meilisearch"; then
-  docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch && cp -r /meilisearch/ ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch"
+docker compose exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/cache && cp -r /redis-data/ ${TEMP_BACKUP_DIR}/volumes-monolithic/cache"
+docker compose exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy && cp -r /caddy/ ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy"
+if docker compose exec -u root supportpal bash -c "test -d /meilisearch"; then
+  docker compose exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch && cp -r /meilisearch/ ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch"
 fi
-if docker exec -u root supportpal bash -c "test -d /qdrant"; then
-    docker exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant && cp -r /qdrant/ ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant"
+if docker compose exec -u root supportpal bash -c "test -d /qdrant"; then
+    docker compose exec -u root supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant && cp -r /qdrant/ ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant"
 fi
 
-echo 'Combining backups...'
-docker exec -u root supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -czf ${APP_BACKUP_NAME} ${FILESYSTEM_BACKUP_NAME} ${DB_FILE_NAME} volumes-monolithic/"
+TMP_DIR="/tmp/backup_$(date +%s)_$RANDOM"
+mkdir -p "$TMP_DIR"
 
-echo 'Copying backup to host...'
-mkdir -p "${BACKUP_DIR}/"
-docker cp "supportpal:${TEMP_BACKUP_DIR}/${APP_BACKUP_NAME}" "${BACKUP_DIR}/"
-docker exec -u root supportpal bash -c "rm -rf ${TEMP_BACKUP_DIR}/"
+echo "Backing up current working directory: $(pwd)..."
+tar -czf "$TMP_DIR/docker-files.tar.gz" --exclude="./${BACKUP_DIR}" .
+
+# Copy files to host.
+execute_command "docker cp ""supportpal:${TEMP_BACKUP_DIR}/${FILESYSTEM_BACKUP_NAME}"" ""${TMP_DIR}"""
+execute_command "docker cp ""supportpal:${TEMP_BACKUP_DIR}/${DB_FILE_NAME}"" ""${TMP_DIR}"""
+execute_command "docker cp ""supportpal:${TEMP_BACKUP_DIR}/volumes-monolithic"" ""${TMP_DIR}"""
+
+# Combine backup files.
+mkdir -p "${ABS_BACKUP_PATH}/"
+(cd "$TMP_DIR" && tar -czf "${ABS_BACKUP_PATH}/${APP_BACKUP_NAME}" "${FILESYSTEM_BACKUP_NAME}" "${DB_FILE_NAME}" volumes-monolithic/ docker-files.tar.gz)
+
+# Cleanup.
+docker compose exec -u root supportpal bash -c "rm -rf ${TEMP_BACKUP_DIR}/"
+rm -rf "$TMP_DIR"
+
+echo "Backup created successfully at ${ABS_BACKUP_PATH}/${APP_BACKUP_NAME}"
+echo
 
 if ! $online; then
   echo "Restarting services..."
-  docker restart supportpal
+  execute_command "docker restart supportpal"
 fi
-
-echo "Backup created successfully at ${PWD}/${BACKUP_DIR}/${APP_BACKUP_NAME}"
--- templates/docker-monolithic/restore.sh
+++ templates/docker-monolithic/restore.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
-
 set -eu -o pipefail
 
-LAST_BACKUP_DIR="backup"
+usage="Options:
+    -h,--help                  Display this help and exit.
+    --debug                    Enable verbose output.
+"
+
+# Options
+debug=false
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  -h|--help) echo "$usage" ; exit 0 ;;
+  --debug) debug=true ;;
+  *)
+    echo "Unknown parameter passed: $1"
+    exit 1
+    ;;
+  esac
+  # shellcheck disable=SC2317
+  shift
+done
+
+if $debug; then
+  set -x
+fi
+
+execute_command() {
+    local cmd="$1"
+
+    if [ "$debug" = false ]; then
+        eval "$cmd > /dev/null 2>&1"
+    else
+        eval "$cmd"
+    fi
+}
+
+LAST_BACKUP_DIR="$(pwd)/backup"
 if [[ ! -d "${LAST_BACKUP_DIR}" ]]; then
   echo "The ${LAST_BACKUP_DIR}/ directory does not exist. Create the directory, add your backup file to it and try again."; exit 1
 fi
@@ -19,37 +53,124 @@ LAST_BACKUP_FILE="$(echo "${LAST_BACKUP_FILE_PATH}" | xargs basename)"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S-%s)
 TEMP_BACKUP_DIR="/tmp/tmp-backups/${TIMESTAMP}"
 
-# Determine command path. app:restore moved to app-manager in v4.2.0
-GTE_v420="$(docker exec supportpal php -r "\$release = require '/var/www/supportpal/config/release.php'; echo (int) version_compare(\$release['version'], '4.2.0', '>=');")"
-if [[ "$GTE_v420" = "0" ]]; then COMMAND_PATH="/var/www/supportpal"; else COMMAND_PATH="/var/www/supportpal/app-manager"; fi
-
 echo "Found ${LAST_BACKUP_FILE}..."
 
+# List the contents of the archive, and if it contains "docker-files" then restore those files.
+if tar -tzf "${LAST_BACKUP_DIR}/$LAST_BACKUP_FILE" 2>/dev/null | grep -qs "^docker-files.tar.gz"; then
+  PARENT_DIR="$(realpath "$(pwd)/../")"
+  RESTORE_PATH="${PARENT_DIR}/supportpal_$(date +%s)_$RANDOM"
+
+  echo "The backup will be restored to $RESTORE_PATH [Y/n]"
+  read -r PROCEED
+  if [ "${PROCEED}" != "Y" ] ; then
+    exit 0
+  fi
+
+  # Check if container already exists.
+  if [[ -n "$(docker ps -a -q -f name=^supportpal$)" ]]; then
+      echo "Container with name 'supportpal' already exists. Do you want to remove that container? [Y/n]"
+      read -r PROCEED
+      if [ "${PROCEED}" != "Y" ] ; then
+        exit 0
+      fi
+
+      echo "Deleting existing 'supportpal' container and associated volumes..."
+
+      execute_command "docker stop supportpal"
+      execute_command "docker rm supportpal"
+      execute_command "docker volume rm supportpal_db"
+      execute_command "docker volume rm supportpal_config"
+      execute_command "docker volume rm supportpal_redis"
+      execute_command "docker volume rm supportpal_storage"
+      execute_command "docker volume rm supportpal_logs"
+      execute_command "docker volume rm supportpal_addons"
+      execute_command "docker volume rm supportpal_caddy"
+      execute_command "docker volume rm supportpal_meilisearch"
+      execute_command "docker volume rm supportpal_qdrant"
+  fi
+
+  echo "Restoring docker files to $RESTORE_PATH..."
+
+  # Extract docker-files.tar.gz from the backup file to the restore path.
+  mkdir -p "$RESTORE_PATH"
+  cp "${LAST_BACKUP_DIR}/$LAST_BACKUP_FILE" "$RESTORE_PATH"
+  (cd "$RESTORE_PATH" && tar -xzf "$LAST_BACKUP_FILE" docker-files.tar.gz)
+  (cd "$RESTORE_PATH" && tar -xzf docker-files.tar.gz && rm -f docker-files.tar.gz)
+
+  cd "$RESTORE_PATH"
+
+  # Create volumes.
+  execute_command "docker compose config --volumes | xargs -I {} docker volume create {}"
+
+  # Start the container.
+  echo "Starting container..."
+  execute_command "docker compose up -d"
+
+  # Wait for container to initialise.
+  CONTAINER_ENV_PATH="/etc/container_environment.sh"
+  docker compose exec -e CONTAINER_ENV_PATH="$CONTAINER_ENV_PATH" supportpal bash -c '
+  echo -n "Waiting for container setup to complete."
+
+  FILE="/etc/container_environment.sh"
+  while [ -z "${MYSQL_PASSWORD+x}" ] || [ -z "${INITIALIZED+x}" ]; do
+    if [ -f "$FILE" ]; then
+      source <(sudo cat "$FILE")
+    fi
+
+    echo -n "."
+    sleep 1
+  done
+
+  echo
+  echo "The restore will ask you to provide database connection details. Use the details below:"
+  echo
+  echo "Host: 127.0.0.1"
+  echo "Port: 3306"
+  echo "Database: $MYSQL_DATABASE"
+  echo "Username: $MYSQL_USER"
+  echo "Password: $MYSQL_PASSWORD"
+  echo
+  '
+fi
+
+# Determine command path. app:restore moved to app-manager in v4.2.0
+GTE_v420="$(docker compose exec supportpal php -r "\$release = require '/var/www/supportpal/config/release.php'; echo (int) version_compare(\$release['version'], '4.2.0', '>=');")"
+if [[ "$GTE_v420" = "0" ]]; then COMMAND_PATH="/var/www/supportpal"; else COMMAND_PATH="/var/www/supportpal/app-manager"; fi
+
 echo "Stopping services..."
-docker exec supportpal bash -c "sudo find -L /etc/service -maxdepth 1 -mindepth 1 -type d ! -name 'redis' ! -name '00redis' ! -name 'mysql' ! -name '00mysql' -printf '%f\n' -exec sv stop {} \;"
+docker compose exec supportpal bash -c "
+    sudo find -L /etc/service -maxdepth 1 -mindepth 1 -type d \
+    ! -name 'redis' ! -name '00redis' ! -name 'mysql' ! -name '00mysql' \
+    -printf '%f\n' -exec sv stop {} \; $(if [ "$debug" = false ]; then echo '> /dev/null 2>&1'; fi)
+  "
 
 echo "Restoring..."
 
-docker exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}"
-docker cp "${LAST_BACKUP_DIR}/${LAST_BACKUP_FILE}" "supportpal:${TEMP_BACKUP_DIR}/"
-TAR_OUTPUT=$(docker exec supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -xvzf ${LAST_BACKUP_FILE}")
-docker exec supportpal bash -c "cd ${COMMAND_PATH} && php artisan app:restore ${TEMP_BACKUP_DIR}/${LAST_BACKUP_FILE} --no-verify --force"
+docker compose exec supportpal bash -c "mkdir -p ${TEMP_BACKUP_DIR}"
+execute_command "docker cp ""${LAST_BACKUP_DIR}/${LAST_BACKUP_FILE}"" ""supportpal:${TEMP_BACKUP_DIR}/"""
+TAR_OUTPUT=$(docker compose exec supportpal bash -c "cd ${TEMP_BACKUP_DIR} && tar -xvzf ${LAST_BACKUP_FILE}")
+docker compose exec supportpal bash -c "cd ${COMMAND_PATH} && php artisan app:restore ${TEMP_BACKUP_DIR}/${LAST_BACKUP_FILE} --no-verify --force"
 
 # If backup generated via docker, restore volumes.
 if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/$'; then
   echo 'Restoring volume data...'
 
-  docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/cache/redis-data /"
-  docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy/caddy /"
+  docker compose exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/cache/redis-data /"
+  docker compose exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/caddy/caddy /"
   if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/meilisearch'; then
-    docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch/meilisearch /"
+    docker compose exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/meilisearch/meilisearch /"
   fi
   if echo "${TAR_OUTPUT}" | grep -qs '^volumes-monolithic/qdrant'; then
-    docker exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant/qdrant /"
+    docker compose exec -u root supportpal bash -c "cp -r ${TEMP_BACKUP_DIR}/volumes-monolithic/qdrant/qdrant /"
   fi
   rm -rf "backup/${TIMESTAMP}/"
 fi
 
 echo "Restarting services..."
-docker compose down
-docker compose up -d
+execute_command "docker compose down"
+execute_command "docker compose up -d"
+
+if [[ -n "${RESTORE_PATH+x}" ]]; then
+  echo
+  echo "Successfully restored to $RESTORE_PATH"
+fi
